3f7ed945066545585a4c774484bed43206d8e363
loicp <loicprenevost@gmail.com>
2018-03-16 11:27:46 -0400

    initial commit

---
+++ clean.py
@@ -0,0 +1,28 @@
+import json
+
+file_txt = open("Dataset.csv",'r').read()
+set(w.lower() for w in file_txt)
+
+#print(file_txt)
+
+
+headers = []
+datas = []
+
+for index, line in enumerate(file_txt.split("\n")):
+    data = {}
+    split = line.split(',')
+    
+    if index == 0 :
+        headers = split
+    else:
+        del split[-1]
+        if (len(headers) > 0):
+            for col, value in enumerate(split):
+                data["" + headers[col]] = value
+            datas.insert(len(datas) - 1, data);
+
+
+print(str(len(datas)) + " entrées")
+
+# Exemple accéder à une entrée : datas[index]['Pays']
