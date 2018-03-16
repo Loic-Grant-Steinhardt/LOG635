8063b8301e80defea1f8aa8cf27be82d4339a91c
loicp <loicprenevost@gmail.com>
2018-03-16 11:41:23 -0400

    Enlever les espaces de debut et de fin

--- clean.py
+++ clean.py
@@ -5,7 +5,6 @@ set(w.lower() for w in file_txt)
 
 #print(file_txt)
 
-
 headers = []
 datas = []
 
@@ -19,7 +18,7 @@ for index, line in enumerate(file_txt.split("\n")):
         del split[-1]
         if (len(headers) > 0):
             for col, value in enumerate(split):
-                data["" + headers[col]] = value
+                data["" + headers[col]] = value.strip()
             datas.insert(len(datas) - 1, data);
 
 
