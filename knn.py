77a1ecab1cd6fe1847eeebb56f49ef0a279726a8
loicp <loicprenevost@gmail.com>
2018-04-02 14:37:24 -0400

    KNN Baby !

--- knn.py
+++ knn.py
@@ -1,4 +1,8 @@
 ### ALGORITHME KNN ###
+import csv
+import random
+import math
+import operator
 
 # Lire les fichiers
 file_txt = open("Dataset.csv", "r", -1, "utf-8").read()
@@ -9,13 +13,15 @@ headers = lines[0].split(',')
 file_txt = open("Evaluations.csv", "r", -1, "utf-8").read()
 set(w.lower() for w in file_txt)
 lines2 = file_txt.split("\n")
-
+headers2 = lines2[0].split(',')
+          
 # Élimination de certaines valeurs
 indexId = headers.index("Id")
 del headers[indexId]
 
-IndexEthni = headers.index("Ethnicité")
-del headers[IndexEthni]
+indexNicotine = headers.index("Nicotine")
+del headers[indexNicotine]
+headers.insert(len(headers), "Nicotine")
 
 # Initialisation de variables
 dataset = []
@@ -60,6 +66,14 @@ educationMultiplier = {
   "À quitté l'école à 16 ans" : 0.88,
   "À quitté l'école avant 16 ans" : 1
 }
+ethniciteMultiplier = {
+    "Blanc": 0.17,
+    "Mixed-Blanc/Asiatique": 0.34,
+    "Mixed-Blanc/Noir": 0.51,
+    "Asiatique": 0.68,
+    "Noir": 0.85,
+    "Autre": 1.00
+}
 
 # Détermine si une valeur est numérique
 def isfloat(value):
@@ -69,11 +83,46 @@ def isfloat(value):
   except ValueError:
     return False
 
+def euclideanDistance(instance1, instance2, length):
+  distance = 0
+  for x in range(length):
+    distance += pow((instance1[x] - instance2[x]), 2)
+  return math.sqrt(distance)
+
+def getNeighbors(dataset, testInstance, k):
+  distances = []
+  length = len(testInstance)-1
+  for x in range(len(dataset)):
+    dist = euclideanDistance(testInstance, dataset[x], length)
+    distances.append((dataset[x], dist))
+  distances.sort(key=operator.itemgetter(1))
+  neighbors = []
+  for x in range(k):
+    neighbors.append(distances[x][0])
+  return neighbors
+
+def getResponse(neighbors):
+  classVotes = {}
+  for x in range(len(neighbors)):
+    response = neighbors[x][-1]
+    if response in classVotes:
+      classVotes[response] += 1
+    else:
+      classVotes[response] = 1
+  sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+  return sortedVotes[0][0]
+
+def getAccuracy(evalSet, predictions):
+  correct = 0
+  for x in range(len(evalSet)):
+    if evalSet[x][-1] == predictions[x]:
+      correct += 1
+  return (correct/float(len(evalSet))) * 100.0
+
 # Ajuster les valeurs entre 0 et 1 selon la méthode avec l'étendu
 def ajustValues(datas):
   for i, data in enumerate(datas):
     for j, value in enumerate(data):
-      #print(value)
       header = headers[j]
       minValue = minMax[''+header]['min']
       maxValue = minMax[''+header]['max']
@@ -94,7 +143,7 @@ def ajustValues(datas):
         datas[i][j] = newValue
   return datas
 
-def createDataset(lines):
+def createDataset(lines, isTraining):
   datas = []
   # Loop sur chaque ligne du fichier
   for index, line in enumerate(lines):
@@ -102,12 +151,14 @@ def createDataset(lines):
 
     # Élimination de certaines valeurs
     del row[indexId]
-    del row[IndexEthni]
 
-    # On skip la première ligne
+    # On skip la première ligne (headers)
     if index > 0 :
       del row[-1]
       valeurAberrante = False
+
+      # Conserver la valeur de la Nicotine
+      nicotineValue = float(row[indexNicotine])
       
       # Loop sur chaque valeur d'une ligne
       for col, value in enumerate(row):
@@ -147,6 +198,15 @@ def createDataset(lines):
             value = genreMultiplier[value]
             row[col] = value
 
+        # Ethnicité impossible
+        if(header == "Ethnicité"):
+          if(value not in ethniciteMultiplier):
+            valeurAberrante = True
+            break
+          else:
+            value = ethniciteMultiplier[value]
+            row[col] = value
+
         # Pays impossible
         if(header == "Pays"):
           if(value not in paysMultiplier):
@@ -156,12 +216,34 @@ def createDataset(lines):
             value = paysMultiplier[value]
             row[col] = value
 
+        # Retirer 'Nicotine' de la row
+        if(col == indexNicotine):
+          value = float(value)
+
+          '''# Un peu sketch mais ca va pour l'instant
+          currentMin = minMax['Nicotine']['min']
+          currentMax = minMax['Nicotine']['max']
+
+          if(currentMin is None):
+            minMax['Nicotine']['min'] = float(value)
+          elif(float(value) < currentMin):
+            minMax['Nicotine']['min'] = float(value)
+
+          if(currentMax is None):
+            minMax['Nicotine']['max'] = float(value)
+          elif(float(value) > currentMax):
+            minMax['Nicotine']['max'] = float(value)'''
+          
+          row.pop(col)
+          #break
+
         # Si la valeur est numerique
         if(isfloat(value)):
           # Convertir string en float
           value = float(value)
           row[col] = value
 
+          # Affecter le tableau minMax si nécessaire
           currentMin = minMax[header]['min']
           currentMax = minMax[header]['max']
 
@@ -176,19 +258,36 @@ def createDataset(lines):
             minMax[header]['max'] = float(value)
 
       # Ajouter la valeur si elle n'est pas aberrante
-      if not valeurAberrante :
+      if not valeurAberrante:
+        # Mettre 'Nicotine' à la fin
+        row.insert(len(row), nicotineValue)
+
+        # Ajouter les données au dataset
         datas.insert(len(datas) - 1, row)
 
+  # Retourner le dataset
   return datas
 
 
 # Training set
-dataset = createDataset(lines)
-dataset = ajustValues(dataset)
+dataset = createDataset(lines, True)
+#dataset = ajustValues(dataset)
 
 # Evaluation set
-evalSet = createDataset(lines2)
-evalSet = ajustValues(evalSet)
+evalSet = createDataset(lines2, False)
+#evalSet = ajustValues(evalSet)
 
-print("Training set length   : " + str(len(dataset)) + " entries")
-print("Evaluation set length : " + str(len(evalSet)) + " entries")
+'''print("Training set length   : " + str(len(dataset)) + " entries")
+print("Evaluation set length : " + str(len(evalSet)) + " entries")'''
+
+predictions=[]
+k = 3
+
+for x in range(len(evalSet)):
+  neighbors = getNeighbors(dataset, evalSet[x], k)
+  result = getResponse(neighbors)
+  predictions.append(result)
+  print('> predicted=' + repr(result) + ', actual=' + repr(evalSet[x][-1]))
+  
+accuracy = getAccuracy(evalSet, predictions)
+print('Accuracy: ' + repr(accuracy) + '%')
